a9c16b50b4dc0c355c17556d2a5aea46fcea7876
JUSTadICE <x1255993080@163.com>
2022-03-31 16:08:45 +0800

    初始化

---
+++ src/main/java/com/example/dblab2/DBlab2Application.java
@@ -0,0 +1,13 @@
+package com.example.dblab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBlab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBlab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dblab2/DBlab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dblab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBlab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
